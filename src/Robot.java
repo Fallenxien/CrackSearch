7713d8318ec063235c63330237a5dcb65e626a47
Andy <andrew.nickells@gmail.com>
2018-03-13 15:38:15 +0000

    Added robot drawing to world screen

--- src/Robot.java
+++ src/Robot.java
@@ -5,6 +5,16 @@
 // A.P.Nickells@student.liverpool.ac.uk
 // University of Liverpool
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import javax.imageio.*;
+import java.util.List;
 /**
  * Robot
  *
@@ -12,5 +22,27 @@
  */
 public class Robot {
 
+    private ExplorationAlgorithm algorithm;
+    private Route r;
 
+    public Robot(List<Crack> cracks) {
+
+
+    }
+
+    /**
+     * Draw the robot to the given 2d graphics object
+     * @param g
+     */
+    public void drawRobot(Graphics2D g) {
+
+        try {
+            BufferedImage img = ImageIO.read(new File("C:\\Users\\Andy\\IdeaProjects\\CrackSearch\\src\\robot.png"));
+            g.drawImage(img,100,100,40,43,null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
